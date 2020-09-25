b1e16b5ee7de1e059e4334b1954e0396779ff6dc
jamei <jnbrag@gmail.com>
2020-09-25 15:28:15 -0300

    exercicios

--- raio.py
+++
@@ -1,8 +0,0 @@
-'''Faça um Programa que peça o raio de um círculo, calcule e mostre sua área.'''
-
-def raio(r):
-    pi=3.14
-    return (pi*(r**2))
-
-r2=float(input("Informe o raio:"))
-print("A área da circuferencia é: %1.4f" %raio(r2))
