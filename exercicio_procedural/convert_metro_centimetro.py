544c7d260c89718fa83bb29b82b904b1d36985c0
jamei <jnbrag@gmail.com>
2020-09-25 15:43:08 -0300

    converte metro em centimetro

--- exercicio_procedural/convert_metro_centimetro.py
+++ exercicio_procedural/convert_metro_centimetro.py
@@ -1,8 +1,7 @@
 #Faça um Programa que converta metros para centímetros.
 
-def convert_m_c (metro):
+def convert_m_cm (metro):
     return (metro * 100)
 
 m= float(input("Informe a metragem desejada:"))
-
-print("O valor em centimetros é: %1.4f" %convert_m_c(m))
+print("O valor em centimetros é: %1.4f" %convert_m_cm(m))
